95bb705e0415c53a3f74e3e54ad19768f64766d8
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-04 21:15:32 +0530

    Time: 9 ms (5.43%) | Memory: 47 MB (5.20%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean check(String s){
+        int count = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == '(') count++;
+            else count--;
+            if(count < 0) return false;
+        }
+        if(count==0)return true;
+        return false; 
+    }
+    public void helper(String otp, List<String> ans, int level, int n){
+        if(level == 2*n){
+            if(check(otp)){
+                ans.add(otp);
+            }
+            return;
+        }
+        helper( otp + '(' , ans , level+1 , n);
+        helper( otp + ')' , ans , level+1 , n);
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        helper("" , ans , 0 , n);
+        return ans;
+    }
+}
