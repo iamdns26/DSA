89ce3c9e605880cefcababaee648fb1cce184bcf
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-04 20:36:21 +0530

    Time: 6 ms (29.30%) | Memory: 52.8 MB (12.66%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -1,31 +1,28 @@
 class Solution {
-    public void helper(String inp, String otp, List<String> ans){
-        if(inp.length()==0){
+    public void helper(String inp, String otp, List<String> ans) {
+        if (inp.length() == 0) {
             ans.add(otp);
             return;
         }
+
         char ch = inp.charAt(0);
-        int ascii = (int)ch;
 
-        if(ch >= 48 && ch <= 57){
-            String newInp = inp.substring(1);
-            String newOut = otp + ch ;
+        // If digit
+        if (Character.isDigit(ch)) {
+            helper(inp.substring(1), otp + ch, ans);
+        } 
+        else {
+            // Lowercase branch
+            helper(inp.substring(1), otp + Character.toLowerCase(ch), ans);
 
-            helper(newInp, newOut, ans);
-        }
-        else{
-            String newInp = inp.substring(1);
-            String newOut1 = otp + Character.toLowerCase(ch);
-            String newOut2 = otp + Character.toUpperCase(ch); 
-
-            helper(newInp, newOut1, ans);
-            helper(newInp, newOut2, ans);
+            // Uppercase branch
+            helper(inp.substring(1), otp + Character.toUpperCase(ch), ans);
         }
     }
+
     public List<String> letterCasePermutation(String s) {
         List<String> ans = new ArrayList<>();
-        String otp = "";
-        helper(s,otp,ans);
+        helper(s, "", ans);
         return ans;
     }
 }
