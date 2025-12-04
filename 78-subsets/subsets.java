2cd45adad6374b5498762c03fe50b9790ba0f15f
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-04 16:01:38 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (25.51%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void helper(int[] arr, int idx, List<Integer> out, List<List<Integer>> ans){
+        if(idx==arr.length){
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0 ; i < out.size() ; i++){
+                list.add(out.get(i));
+            }
+            ans.add(list);
+            return;
+        }
+        List<Integer> list1 = new ArrayList<>(out);
+        List<Integer> list2 = new ArrayList<>(out);
+        list2.add(arr[idx]);
+
+        helper(arr,idx+1,list1,ans);
+
+        helper(arr,idx+1,list2,ans);
+
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        helper(nums,0,list,ans);
+        return ans;
+    }
+}
