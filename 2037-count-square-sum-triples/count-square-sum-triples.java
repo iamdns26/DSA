be1272470bd5fba4512f6c7fe828bcfc8283081c
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-09 01:40:58 +0530

    Time: 98 ms (18.67%) | Memory: 42.4 MB (18.67%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -5,7 +5,7 @@ class Solution {
             arr[i-1] = i*i;
         }
         int count = 0;
-        for(int i = 1 ; i <= n ; i++){
+        for(int i = 0 ; i < n ; i++){
             for(int j = i+1 ; j < n ; j++){
                 for(int k = j+1 ; k < n ; k++){
                     if(arr[i] + arr[j] == arr[k]) count+=2;
