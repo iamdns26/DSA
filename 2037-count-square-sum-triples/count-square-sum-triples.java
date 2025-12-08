9085a3121a012c7fd376a05440a43e5e2dc68f7d
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-09 01:38:37 +0530

    Time: 78 ms (19.50%) | Memory: 42.3 MB (26.97%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countTriples(int n) {
+        int[] arr = new int[n];
+        for(int i = 1 ; i <= n ; i++){
+            arr[i-1] = i*i;
+        }
+        int count = 0;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                for(int k = j+1 ; k < n ; k++){
+                    if(arr[i] + arr[j] == arr[k]) count+=2;
+                }
+            }
+        }
+        return count;
+    }
+}
