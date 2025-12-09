9e68c3bb8c4f128f17850274b892141629ab62e0
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-09 10:58:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (50.46%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int helper(int i, int j,int[][] dp) {
+        if (i < 0 || j < 0) return 0;
+        if (i == 0 && j == 0) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = helper(i - 1, j,dp);
+        int left = helper(i, j - 1,dp);
+
+        return dp[i][j] = up + left;
+    }
+    public int uniquePaths(int m, int n) {
+        int[][]dp = new int[m+1][n+1];
+         for(int[] d : dp) Arrays.fill(d,-1);
+        return helper(m-1 , n-1 , dp);
+    }
+
+}
+
