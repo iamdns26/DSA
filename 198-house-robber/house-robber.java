a31bed0f35ba053a73cd3c5fff83194f66dac52b
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-16 21:30:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (36.51%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,28 +1,14 @@
 class Solution {
-    //  int helper(int[] arr, int idx,int[] dp){
-    //     if (idx >= arr.length) return 0;
-    //     if(dp[idx] != -1) return dp[idx];
-        
-    //     int v1 = arr[idx] + helper(arr,idx+2,dp); 
-
-    //     int v2 = helper(arr,idx+1,dp); 
-        
-    //     return dp[idx] = Math.max(v1,v2);
-    // }
+     public int robHelper(int[] nums, int idx, int[] dp) {
+        if(idx >= nums.length) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        int take = nums[idx] + robHelper(nums,idx+2,dp);
+        int skip = robHelper(nums,idx+1,dp);
+        return dp[idx] = Math.max(take,skip);
+    }
     public int rob(int[] nums) {
         int[] dp = new int[nums.length];
-        int n = nums.length;
         Arrays.fill(dp,-1);
-        //return helper(nums,0,dp);
-        if(n==1) return nums[0];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
-        
-        for(int i = 2 ; i < nums.length ; i++){
-            int v1 = nums[i] + dp[i-2];
-            int v2 = dp[i-1];
-            dp[i] = Math.max(v1,v2);
-        }
-        return dp[n-1];
+        return robHelper(nums,0,dp);
     }
 }
