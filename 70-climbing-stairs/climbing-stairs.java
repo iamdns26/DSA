c0b1b014bf81556642aaa272fe5e4248511ce83b
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-16 21:00:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (6.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,12 +1,15 @@
 class Solution {
-    public int climbStairsHelper(int n,int[] dp) {
-        if(n==1) return 1;
-        if(n==2) return 2;
-        if(dp[n]!=0) return dp[n];
-        return dp[n] = climbStairsHelper(n-1,dp) + climbStairsHelper(n-2,dp);
+    public int climbStairsHelper(int idx, int n, int[] dp) {
+        if(idx==n) return 1;
+        if(idx > n) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        int one = climbStairsHelper(idx+1,n,dp);
+        int two = climbStairsHelper(idx+2,n,dp);
+        return dp[idx] = one + two ;
     }
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
-        return climbStairsHelper(n,dp);
+        Arrays.fill(dp,-1);
+        return climbStairsHelper(0,n,dp);
     }
 }
