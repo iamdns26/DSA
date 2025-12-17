69d02dd6a1a71ce3c264fb9bb7bebd2b1f1a080f
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-18 02:25:55 +0530

    Time: 6 ms (65.69%) | Memory: 66.8 MB (33.47%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.java
@@ -1,25 +1,18 @@
 class Solution {
-    public int minSubArray(int[] nums) {
-        int sum = 0;
-        int mnSum = Integer.MAX_VALUE; 
-        for(int ele : nums){
-            sum+=ele;
-            mnSum = Math.min(sum,mnSum);
-            if(sum>0) sum = 0;
-        }
-        return mnSum;
-    }
-    public int maxSubArray(int[] nums) {
-        int sum = 0;
-        int mxSum = Integer.MIN_VALUE; 
-        for(int ele : nums){
-            sum+=ele;
-            mxSum = Math.max(sum,mxSum);
-            if(sum<0) sum = 0;
-        }
-        return mxSum;
-    }
     public int maxAbsoluteSum(int[] nums) {
-        return Math.max(Math.abs(maxSubArray(nums)) , Math.abs(minSubArray(nums)));
+        
+        int maxSum = nums[0], curMax = 0;
+        int minSum = nums[0], curMin = 0;
+
+        for (int num : nums) {
+            curMax = Math.max(num, curMax + num);
+            maxSum = Math.max(maxSum, curMax);
+
+            curMin = Math.min(num, curMin + num);
+            minSum = Math.min(minSum, curMin);
+        }
+
+        return Math.max(maxSum,Math.abs(minSum));
+       
     }
 }
