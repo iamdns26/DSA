121f064917666f423a9d86105d1ad7847e744662
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-18 00:00:31 +0530

    Time: 6 ms (79.09%) | Memory: 50.9 MB (58.64%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,30 +1,23 @@
 class Solution {
-    public int minSubArray(int[] nums) {
-        int sum = 0;
-        int mnSum = Integer.MAX_VALUE; 
-        for(int ele : nums){
-            sum+=ele;
-            mnSum = Math.min(sum,mnSum);
-            if(sum>0) sum = 0;
-        }
-        return mnSum;
-    }
-    public int maxSubArray(int[] nums) {
-        int sum = 0;
-        int mxSum = Integer.MIN_VALUE; 
-        for(int ele : nums){
-            sum+=ele;
-            mxSum = Math.max(sum,mxSum);
-            if(sum<0) sum = 0;
-        }
-        return mxSum;
-    }
     public int maxSubarraySumCircular(int[] nums) {
-        int sum = 0;
-        for(int ele : nums) sum+=ele;
-        int maxSub = maxSubArray(nums);
-        int minSub = minSubArray(nums);
-        if(maxSub <= 0) return maxSubArray(nums);
-        return Math.max(maxSub, sum-minSub);
+        int totalSum = 0;
+
+        int maxSum = nums[0], curMax = 0;
+        int minSum = nums[0], curMin = 0;
+
+        for (int num : nums) {
+            curMax = Math.max(num, curMax + num);
+            maxSum = Math.max(maxSum, curMax);
+
+            curMin = Math.min(num, curMin + num);
+            minSum = Math.min(minSum, curMin);
+
+            totalSum += num;
+        }
+        // All numbers negative case
+        if (maxSum < 0) {
+            return maxSum;
+        }
+        return Math.max(maxSum, totalSum - minSum);
     }
 }
