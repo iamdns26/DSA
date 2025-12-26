74b973ec2d2b09b63b05c26c58c91670849b4d83
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-27 00:48:49 +0530

    Time: 10 ms (99.30%) | Memory: 75.4 MB (20.35%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean subsetSum(int n, int target, int[] arr, Boolean[][] dp){
-        if(target==0) return true;
-        if(n==0) return false;
+        if(n==0){
+            if(target==0) return true;
+            else return false;
+        }
         if(dp[n][target] != null) return dp[n][target];
         if(arr[n-1] <= target){
             return dp[n][target] = subsetSum(n-1,target-arr[n-1],arr,dp) || subsetSum(n-1,target,arr,dp) ;
@@ -14,7 +16,6 @@ class Solution {
         for(int ele : nums) sum+=ele;
         if(sum%2 != 0) return false;
         Boolean[][] dp = new Boolean[n+1][sum/2+1];
-
         return subsetSum(n,sum/2,nums,dp);
     }
 }
