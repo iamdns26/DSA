26072dc3a266112030feda031a9f992ab75239aa
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2025-12-27 00:33:47 +0530

    Time: 15 ms (96.89%) | Memory: 75.8 MB (16.75%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean subsetSum(int n, int target, int[] arr, Boolean[][] dp){
+        if(target==0) return true;
+        if(n==0) return false;
+        if(dp[n][target] != null) return dp[n][target];
+        if(arr[n-1] <= target){
+            return dp[n][target] = subsetSum(n-1,target-arr[n-1],arr,dp) || subsetSum(n-1,target,arr,dp) ;
+        }
+        else return dp[n][target] = subsetSum(n-1,target,arr,dp);
+    }
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for(int ele : nums) sum+=ele;
+        if(sum%2 != 0) return false;
+        Boolean[][] dp = new Boolean[n+1][sum/2+1];
+
+        return subsetSum(n,sum/2,nums,dp);
+    }
+}
