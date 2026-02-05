a2fa5acbe93ad11c8668a4c67819dab708bd6e16
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-02-05 21:09:41 +0530

    Time: 56 ms (31.95%) | Memory: 99.3 MB (29.00%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -4,14 +4,16 @@ class Solution {
         int i = 0, j = 0;
         long sum = 0, mxSum = 0;
         Map<Integer, Integer> map = new HashMap<>();
-        while (j < n) {
+        while (j < n) { 
+            // -->> calculation Part
             // add arr[j]
             sum += arr[j];
             map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
 
-            if(j - i + 1 < k) j++;
+            if(j - i + 1 < k) j++; // -->> adjusting the window 
 
             else if (j - i + 1 == k) {
+                // -->> part of calculating ans
                 if (map.size() == k) {
                     mxSum = Math.max(mxSum, sum);
                 }
@@ -21,6 +23,8 @@ class Solution {
                     map.remove(arr[i]);
                 }
                 sum -= arr[i];
+
+                // -->> sifting the window
                 i++;
                 j++;
             }
