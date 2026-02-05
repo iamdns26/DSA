dad8630d6104503262bad74f3530b3d7b4880cf4
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-02-05 21:05:41 +0530

    Time: 58 ms (21.92%) | Memory: 99.3 MB (29.00%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,28 +1,27 @@
 class Solution {
     public long maximumSubarraySum(int[] arr, int k) {
         int n = arr.length;
-        int i = 0;
+        int i = 0, j = 0;
         long sum = 0, mxSum = 0;
-
         Map<Integer, Integer> map = new HashMap<>();
-
-        for (int j = 0; j < n; j++) {
-
+        while (j < n) {
+            // add arr[j]
             sum += arr[j];
             map.put(arr[j], map.getOrDefault(arr[j], 0) + 1);
 
             if (j - i + 1 == k) {
-
                 if (map.size() == k) {
                     mxSum = Math.max(mxSum, sum);
                 }
-
+                // remove arr[i]
                 map.put(arr[i], map.get(arr[i]) - 1);
-                if (map.get(arr[i]) == 0) map.remove(arr[i]);
-
+                if (map.get(arr[i]) == 0) {
+                    map.remove(arr[i]);
+                }
                 sum -= arr[i];
                 i++;
             }
+            j++; // ALWAYS move j
         }
         return mxSum;
     }
