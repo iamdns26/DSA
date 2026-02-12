f33ffc2fb39dc9dc54da61db6d6b0756bd9ca76f
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-02-13 02:57:54 +0530

    Time: 28 ms (98.37%) | Memory: 47.1 MB (27.38%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestBalanced(String s) {
+        char[] array = s.toCharArray();
+        int[] freq = null;
+        int n = array.length;
+        int ans = 1;
+        for (int i = 0; i < n - 1; i++) {
+            freq = new int[128];
+            int distinct = 0;
+            int maxFreq = 1;
+            for (int j = i; j < n; j++) {
+                freq[array[j]]++;
+                if (freq[array[j]] == 1) {
+                    distinct++;
+                }
+                maxFreq = Math.max(maxFreq, freq[array[j]]);
+                if ((j - i + 1) == maxFreq * distinct) {
+                    ans = Math.max(ans, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+}
