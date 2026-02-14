a1a0fea40e8bdab75f16d20b12d7f5c02868d1e2
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-02-15 03:16:59 +0530

    Time: 3 ms (76.62%) | Memory: 48.2 MB (70.14%) - LeetSync

--- 815-champagne-tower/champagne-tower.java
+++ 815-champagne-tower/champagne-tower.java
@@ -1,26 +1,18 @@
 class Solution {
-    public double champagneTower(int poured, int query_row, int query_glass) {
-        
-        double[][] dp = new double[101][101];
-        dp[0][0] = poured;
-
-        for(int row= 0;row < 100;row++){
-            for(int col = 0;col<=row;col++){
-
-                if(dp[row][col] > 1){
-                    
-                    double overflow = (dp[row][col]-1)/2.0;
-
-                    dp[row+1][col] += overflow;
-                    dp[row+1][col+1] += overflow;
-
-                    dp[row][col] = 1;
-
-                }
-            }
-        }
-
-        return Math.min(1, dp[query_row][query_glass]);
-
+    Double dp[][];
+    public double helper(int p , int r , int c){
+        if(r < 0 || c < 0 || r < c) return 0.0;
+        if(r == 0 && c == 0) return p;
+        if(dp[r][c] != null) return dp[r][c];
+        double left = (helper(p , r-1 , c-1) - 1)/2.0;
+        double right = (helper(p , r-1, c) - 1)/2.0;
+        if(left < 0) left = 0.0;
+        if(right < 0) right = 0.0;
+        return dp[r][c] =  left + right;
     }
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        dp = new Double[101][101];
+        return Math.min(1.0 , helper(poured , query_row , query_glass));
+    }
+    
 }
