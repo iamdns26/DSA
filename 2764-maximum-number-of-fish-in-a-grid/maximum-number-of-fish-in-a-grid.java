129c983e3eadbdb1b04414d91ed582f6354abd4e
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-02-17 15:55:02 +0530

    Time: 4 ms (67.23%) | Memory: 47.1 MB (27.56%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -1,22 +1,24 @@
 class Solution {
-    public int dfs(int i, int j, int[][] grid){
+    public int dfs(int i, int j, int[][] grid,int[][] dir){
         int n = grid.length , m = grid[0].length;
         if(i<0 || j<0 || i>=n || j>=m || grid[i][j]==0) return 0;
         int sum = grid[i][j];
         grid[i][j] = 0;
-        sum += dfs(i+1,j,grid);
-        sum += dfs(i-1,j,grid);
-        sum += dfs(i,j+1,grid);
-        sum += dfs(i,j-1,grid);
+
+        for(int[] d : dir){
+            int nr = i+d[0] , nc = j+d[1];
+            sum += dfs(nr,nc,grid,dir);
+        }
         return sum;
     }
     public int findMaxFish(int[][] grid) {
         int n = grid.length , m = grid[0].length;
         int ans = 0;
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}} ;
         for(int i = 0 ; i < n ; i++){
             for(int j= 0 ; j < m ; j++){
                 if(grid[i][j] != 0){
-                    ans = Math.max(ans,dfs(i,j,grid));
+                    ans = Math.max(ans,dfs(i,j,grid,dir));
                 }
             }
         }
