4d79788215c8da9c09202a6d4f6883eee8f44cbb
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-02-26 11:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (12.15%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public int helper(TreeNode root){
+        if(root == null) return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        return 1 + Math.max(left,right);
+    }
+    public int maxDepth(TreeNode root) {
+        return helper(root);
+    }
+}
