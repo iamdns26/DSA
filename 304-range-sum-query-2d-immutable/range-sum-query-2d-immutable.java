d28530f0a848a491c15476902923ae1bddde4434
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-03-09 12:40:04 +0530

    Time: 101 ms (83.37%) | Memory: 142.1 MB (43.90%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,14 +1,12 @@
 class NumMatrix {
-    int[][] arr;
     int[][] pre;
     public NumMatrix(int[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        this.arr = matrix;
         pre = new int[n+1][m+1];
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= m ; j++){
-                pre[i][j] = arr[i-1][j-1] + pre[i-1][j] + pre[i][j-1] - pre[i-1][j-1];
+                pre[i][j] = matrix[i-1][j-1] + pre[i-1][j] + pre[i][j-1] - pre[i-1][j-1];
             }
         }
     }
