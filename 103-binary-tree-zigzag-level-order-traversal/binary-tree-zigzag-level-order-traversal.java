8c3dd425d0f21c4cc42b2cdd8a81a02e1059c8ba
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-03-18 09:54:00 +0530

    Time: 1 ms (77.84%) | Memory: 43.8 MB (56.98%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,31 +1,32 @@
-
 class Solution {
-    public int level(TreeNode root){
-        if(root==null) return 0;
-        return 1+Math.max(level(root.left),level(root.right));
-    }
-    public void oddPrint(TreeNode root,int level,int n ,List arr){
-        if(root==null) return;
-        if(level>n) return;
-        if(level==n) arr.add(root.val);
-        oddPrint(root.right,level+1,n,arr);
-        oddPrint(root.left,level+1,n,arr);
-    }
-    public void evenPrint(TreeNode root,int level,int n,List arr){
-        if(root==null) return;
-        if(level>n) return;
-        if(level==n) arr.add(root.val);
-        evenPrint(root.left,level+1,n,arr); 
-        evenPrint(root.right,level+1,n,arr);
-    }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        int lvl = level(root);
+
+        Queue<TreeNode> q = new ArrayDeque<>();
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0 ; i<lvl ; i++){
-            List<Integer> arr = new ArrayList<>();
-            if(i%2==0) evenPrint(root,0,i,arr);
-            else oddPrint(root,0,i,arr);
-            ans.add(arr);
+
+        if(root==null) return ans;
+
+        q.add(root);
+        boolean flag = true;
+
+        while(q.size() > 0){
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i = 0 ; i < size ; i++){
+                TreeNode r = q.remove();
+                list.add(r.val);
+                if(r.left != null) q.add(r.left);
+                if(r.right != null) q.add(r.right);
+            }
+            if(flag){
+                ans.add(list);
+                flag = false;
+            }
+            else {
+                Collections.reverse(list);
+                ans.add(list);
+                flag = true;
+            }
         }
         return ans;
     }
