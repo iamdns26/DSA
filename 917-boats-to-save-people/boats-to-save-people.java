78eb17f7d2cb30f2767922123874c448769cc8d3
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-03-19 15:55:52 +0530

    Time: 20 ms (83.12%) | Memory: 56.3 MB (86.55%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -2,25 +2,16 @@ class Solution {
     public int numRescueBoats(int[] arr, int limit) {
         Arrays.sort(arr);
         int n = arr.length;
-
         int i = 0, j = n-1;
         int count = 0;
-
         while(i <= j){
            if(arr[i] + arr[j] <= limit){
             i++;
             j--;
-            count++;
            }
-           else if(arr[i] < arr[j]){
-            j--;
-            count++;
-           }
-           else {
-            count++;
-            i++;
-           }
-           
+           else if(arr[i] < arr[j]) j--;
+           else  i++;
+           count++;
         }
         return count;
     }
