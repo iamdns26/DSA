293fb61f0793930bb931aed98711406271ac54b5
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-04-08 11:51:04 +0530

    Time: 8 ms (53.04%) | Memory: 47.4 MB (93.45%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long minimumSteps(String s) {
+        long count = 0;
+        int one = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == '1') one++;
+            else count += one;
+        }
+        return count;
+    }
+}
