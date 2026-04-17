e8db99ed60e0b02587143d7ebe67751fa69da49e
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-04-18 04:46:40 +0530

    Time: 5 ms (61.74%) | Memory: 44.3 MB (84.12%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,15 +3,14 @@ class Solution {
         int n = s.length() , m = t.length();
         if(n != m) return false;
         int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
         for(int i = 0 ; i < n ; i++){
             int ch1 = (int)s.charAt(i) - 'a';
             int ch2 = (int)t.charAt(i) - 'a';
             freq1[ch1]++;
-            freq2[ch2]++;
+            freq1[ch2]--;
         }
         for(int i = 0 ; i < 26 ; i++){
-            if(freq1[i] != freq2[i]) return false;
+            if(freq1[i] != 0) return false;
         }
         return true;
     }
