dd8423b4ffdb09f282b47237e50bc98b8c61e0d4
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-04-18 18:01:37 +0530

    Time: 5 ms (28.07%) | Memory: 51.3 MB (93.43%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,26 +1,36 @@
 class Solution {
-    public void bfs(int i, int j, boolean[][] vis, char[][] grid){
+    public void bfs(int i, int j, char[][] grid){
         int n = grid.length , m = grid[0].length;
-        if(i < 0 || j < 0 || i >= n || j >= m) return;
-        if(vis[i][j]) return;
-        if(grid[i][j]=='0') return;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i,j});
+        grid[i][j] = '0';
 
-        vis[i][j] = true;
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
 
-        bfs(i+1,j,vis,grid);
-        bfs(i-1,j,vis,grid);
-        bfs(i,j+1,vis,grid);
-        bfs(i,j-1,vis,grid);
+        while(q.size()>0){
+            int[] node = q.remove();
+            int r = node[0];
+            int c = node[1];
 
+            for(int[] d : dir){
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if(nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == '1'){
+                    q.add(new int[]{nr,nc});
+                    grid[nr][nc] = '0';
+                }
+            }
+        }
     }
     public int numIslands(char[][] grid) {
-        int n = grid.length , m = grid[0].length ;
-        boolean[][] vis = new boolean[n][m];
+        int n = grid.length , m = grid[0].length;
+        Queue<int[]> q = new LinkedList<>();
         int count = 0;
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
-                    bfs(i,j,vis,grid);
+                if(grid[i][j]=='1'){
+                    bfs(i,j,grid);
                     count++;
                 }
             }
