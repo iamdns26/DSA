2eefc302470dccba71012f557fec343a3e240ddc
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-04-18 23:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (39.97%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int climbStairsHelper(int n,int[] dp) {
-        if(n<0) return 0;
-        if(n==0) return 1;
+        if(n==1) return 1;
+        if(n==2) return 2;
         if(dp[n] != -1) return dp[n];
         int one = climbStairsHelper(n-1,dp);
         int two = climbStairsHelper(n-2,dp);
