cceba74fa70689f6a0b3d4bd924a2309cbed840c
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-04-19 15:22:42 +0530

    Time: 6 ms (73.87%) | Memory: 47.1 MB (50.76%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,37 +1,32 @@
 class Solution {
-    public boolean dfs(int src, boolean[] vis, boolean[] path,List<List<Integer>> adj){
-        vis[src] = true;
-        path[src] = true;
-
-        for(int neigh : adj.get(src)){
-            if(path[neigh]) return true;
-
-            if(!vis[neigh]){
-                if(dfs(neigh,vis,path,adj)) return true;
-            }
-        }
-        path[src] = false;
-
-        return false;
-    }
     public boolean canFinish(int course, int[][] pre) {
         List<List<Integer>> adj = new ArrayList<>();
 
         for(int i = 0 ; i < course ; i++) adj.add(new ArrayList<>());
+        int[] in = new int[course];
 
         for(int[] e : pre){
             int u = e[0];
             int v = e[1];
             adj.get(u).add(v);
+            in[v]++;
         }
 
-        boolean[] vis = new boolean[course];
-        boolean[] path = new boolean[course];
+        Queue<Integer> qu = new LinkedList<>();
         for(int i = 0 ; i < course ; i++){
-            
-            if(dfs(i,vis,path,adj)) return false;
+            if(in[i]==0) qu.add(i);
+        }
+        int count = 0;
+        while(qu.size() > 0){
+            int par = qu.remove();
+            count++;
+
+            for(int neigh : adj.get(par)){
+                in[neigh]--;
+                if(in[neigh]==0) qu.add(neigh);
+            }
         }
         
-        return true;
+        return count==course;
     }
 }
