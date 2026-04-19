98bb81b6f6b49c53f800c5ed056d16a6d8a837ad
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-04-19 17:28:39 +0530

    Time: 6 ms (86.25%) | Memory: 46.5 MB (55.56%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -9,40 +9,52 @@ class Solution {
     }
     class Triplet{
         int node;
-        int cost;
+        int dist;
         int count;
-        Triplet(int node, int cost, int count){
+        Triplet(int node, int dist, int count){
             this.node = node;
-            this.cost = cost;
+            this.dist = dist;
             this.count = count;
         }
     }
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         List<List<Pair>> adj = new ArrayList<>();
         for(int i = 0 ; i < n ; i++) adj.add(new ArrayList<>());
-        for(int i = 0 ; i < flights.length ; i++){
-            int u = flights[i][0], v = flights[i][1], cost = flights[i][2];
-            adj.get(u).add(new Pair(v,cost));
+        for(int[] e : flights){
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+            adj.get(u).add(new Pair(v,w));
         }
-        int[] ans = new int[n];
-        Arrays.fill(ans,Integer.MAX_VALUE);
-        ans[src] = 0;
-        Queue<Triplet> pq = new LinkedList<>();
-        pq.add(new Triplet(src,0,0));
 
-        while(pq.size() > 0){
-            Triplet t = pq.remove();
-            int node = t.node , cost = t.cost , count = t.count;
-            if(count==k+1) break;
-            for(Pair p : adj.get(node)){
-                int newCost = p.cost + cost;
-                if(newCost < ans[p.node]){
-                    ans[p.node] = newCost;
-                    pq.add(new Triplet(p.node,newCost,count+1));
-                }
+
+        int[] dist = new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        Queue<Triplet> q = new LinkedList<>();
+        q.add(new Triplet(src,0,0));
+
+        while(q.size() > 0){
+            Triplet t = q.remove();
+            int node = t.node , dis = t.dist , count = t.count;
+
+            if(count == k+1) break;
+            //if(dis > dist[node]) continue;
+
+            for(Pair neigh : adj.get(node)){
+
+               int total = neigh.cost + dis;
+               if(total < dist[neigh.node]){
+                 dist[neigh.node] = total;
+                 q.add(new Triplet(neigh.node,total,count+1));
+               }
+
             }
+
         }
-        if(ans[dst] == Integer.MAX_VALUE) return -1;
-        return ans[dst];
+        if(dist[dst] == Integer.MAX_VALUE) return -1;
+        return dist[dst];
+
     }
-} 
+}
