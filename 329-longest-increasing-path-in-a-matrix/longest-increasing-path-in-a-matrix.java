d4de554ffc6351845ee088bce0623e50ff91e5cf
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-04-20 19:15:25 +0530

    Time: 7 ms (97.80%) | Memory: 47 MB (67.84%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,33 +1,33 @@
 class Solution {
+    public int dfs(int i, int j,int par, int[][] dp, int[][] mat){
+
+        int n = dp.length , m = dp[0].length;
+
+        if(i<0 || j < 0 || i >= n ||  j >= m || mat[i][j] <= par) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        par = mat[i][j];
+
+        int left = dfs(i+1,j,par,dp,mat);
+        int down = dfs(i,j+1,par,dp,mat);
+        int up = dfs(i-1,j,par,dp,mat);
+        int right = dfs(i,j-1,par,dp,mat);
+
+        return dp[i][j] = 1 + Math.max(Math.max(left,right), Math.max(down,up));
+    }
     public int longestIncreasingPath(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] dp = new int[m][n];
-        for (int d[] : dp) Arrays.fill(d, -1);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (dp[i][j] == -1) dfs(matrix, dp, m, n, i, j, -1);
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for(int[] d : dp) Arrays.fill(d,-1);
+
+        for(int i = 0 ; i < matrix.length ; i++){
+            for(int j = 0 ; j < matrix[0].length ; j++){
+                if(dp[i][j] == -1) dfs(i,j,Integer.MIN_VALUE,dp,matrix);
             }
         }
         int max = Integer.MIN_VALUE;
-        for (int[] d : dp) {
-            for (int ele : d) max = Math.max(ele, max);
+        for(int[] d : dp){
+            for(int ele : d) max = Math.max(ele,max);
         }
         return max;
-    }
-
-    public int dfs(int[][] matrix, int[][] dp, int m, int n, int i, int j, int parent) {
-        if (i >= m || j >= n || i < 0 || j < 0 || matrix[i][j] <= parent) return 0;
-        parent = matrix[i][j];
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int left = dfs(matrix, dp, m, n, i, j - 1, parent);
-        int right = dfs(matrix, dp, m, n, i, j + 1, parent);
-        int bottom = dfs(matrix, dp, m, n, i + 1, j, parent);
-        int top = dfs(matrix, dp, m, n, i - 1, j, parent);
-        
-        dp[i][j] = 1 + Math.max(Math.max(left, right), Math.max(top, bottom));
-        return dp[i][j];
     }
 }
