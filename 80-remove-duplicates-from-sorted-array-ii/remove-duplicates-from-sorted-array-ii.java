8a69243fc528a0b096f0f254b396f4e7e3f63b19
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-04-23 18:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (53.51%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -2,12 +2,16 @@ class Solution {
     public int removeDuplicates(int[] arr) {
         int n = arr.length;
         int i = 1;
+        int j = 1;
         int count = 1;
-        for(int j = 1 ;j < n ; j++){
+        while(j<n){
+
             if(arr[j]==arr[j-1]) count++;
             else count = 1;
 
             if(count<=2) arr[i++] = arr[j];
+
+            j++;
         }
         return i;
     }
