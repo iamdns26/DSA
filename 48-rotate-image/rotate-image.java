ccfa975de7e5330d7b9f48684e973bd59f97309f
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-05-04 16:01:46 +0530

    Time: 3 ms (1.64%) | Memory: 44 MB (16.04%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+        List<List<Integer>> hel = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++){
+            List<Integer> a = new ArrayList<>();
+            hel.add(a);
+        }
+        for(int j = 0 ; j < n ; j++){
+            for(int i = 0 ; i < n ; i++){
+                hel.get(j).add(mat[i][j]);
+            }
+        }
+        for(int i = 0 ; i <n ; i++){
+            Collections.reverse(hel.get(i));
+        }
+        int x=0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                mat[i][j] = hel.get(i).get(j);
+            }
+        }
+        System.out.print(hel);
+    }
+}
