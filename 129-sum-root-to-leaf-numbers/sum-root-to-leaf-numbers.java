acad99e7cd70367b5be8b3998c8269efaff4426b
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-05-05 10:25:40 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (30.20%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -1,23 +1,19 @@
 
 class Solution {
-    public void helper(TreeNode root, int s, List<Integer> ans) {
+    public void helper(TreeNode root, int s, int[] ans) {
         if(root==null) return;
         if(root.left == null && root.right==null){
             s = s * 10 + root.val;
-            ans.add(s);
+            ans[0] += s;
         }
         s = s * 10 + root.val;
         helper(root.left,s,ans);
         helper(root.right,s,ans);
     }
     public int binaryTreePaths(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
+        int[] ans = {0};
         helper(root,0,ans);
-        int sum = 0;
-        for(int i = 0 ; i < ans.size() ; i++){
-            sum+=ans.get(i);
-        }
-        return sum;
+        return ans[0];
     }
     public int sumNumbers(TreeNode root) {
         return binaryTreePaths(root);
