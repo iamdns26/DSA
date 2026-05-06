57bd926943d3b95e0791529a0c243c6283f1bc94
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-05-06 12:18:08 +0530

    Time: 2 ms (45.26%) | Memory: 45.3 MB (18.34%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -1,33 +1,35 @@
 class Solution {
-    public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
-        
-        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
-        queue.add(new Pair<>(root, 0));
-        int maxWidth = 0;
-        
-        while (!queue.isEmpty()) {
-            int levelLength = queue.size();
-            int levelStart = queue.peek().getValue();
-            int index = 0;
-            
-            for (int i = 0; i < levelLength; i++) {
-                Pair<TreeNode, Integer> pair = queue.poll();
-                TreeNode node = pair.getKey();
-                index = pair.getValue();
-                
-                if (node.left != null) {
-                    queue.add(new Pair<>(node.left, 2*index));
-                }
-                
-                if (node.right != null) {
-                    queue.add(new Pair<>(node.right, 2*index+1));
-                }
-            }
-            
-            maxWidth = Math.max(maxWidth, index - levelStart + 1);
+    class Pair{
+        TreeNode node;
+        int idx;
+        Pair(TreeNode node, int idx){
+            this.node = node;
+            this.idx = idx;
         }
-        
-        return maxWidth;
+    }
+    public int widthOfBinaryTree(TreeNode root) {
+        int max = 0;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root,0));
+
+        while(q.size() > 0){
+            int size = q.size();
+            int st = 0, en = 0;
+
+            for(int i = 0 ; i < size ; i++){
+                Pair p = q.remove();
+                TreeNode node = p.node;
+                int idx = p.idx;
+
+                if(i==0) st = idx;
+                if(i==size-1) en = idx;
+
+                if(node.left != null) q.add(new Pair(node.left, 2 * idx));
+                if(node.right != null) q.add(new Pair(node.right, 2 * idx +1 ));
+
+            }
+            max = Math.max(max,en-st+1);
+        }
+        return max;
     }
 }
