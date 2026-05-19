130423a7f1de9906724844ab4b7fbad2e457cbb5
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-05-19 10:14:56 +0530

    Time: 14 ms (3.92%) | Memory: 48 MB (96.27%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,17 +1,20 @@
 class Solution {
-    public int removeDuplicates(int[] arr) {
-        int n = arr.length;
-        int i = 1;
-        int j = 1;
-        int count = 1;
-        while(j<n){
-
-            if(arr[j]==arr[j-1]) count++;
-            else count = 1;
-
-            if(count<=2) arr[i++] = arr[j];
-
-            j++;
+    public int removeDuplicates(int[] nums) {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int ele : nums){
+            if(map.containsKey(ele)){
+                map.put(ele,map.get(ele)+1);
+            }
+            else map.put(ele,1);
+        }
+        int i = 0;
+        for(int key : map.keySet()){
+            int freq = map.get(key);
+            if(freq==1) nums[i++] = key;
+            else {
+                nums[i++] = key;
+                nums[i++] = key;
+            }
         }
         return i;
     }
