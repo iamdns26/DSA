141c8ab2133cd76124a70bd17659f7fbbe366001
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-05-20 11:55:55 +0530

    Time: 33 ms (68.43%) | Memory: 48.1 MB (56.31%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String removeStars(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            if(ch != '*') sb.append(ch);
+            else if(ch == '*' && sb.length() > 0) sb.deleteCharAt(sb.length()-1);
+        }
+        return sb.toString();
+    }
+}
