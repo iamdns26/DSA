38418930d4ccc691b9dc4eb2957fa97e64a010a9
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-05-25 09:52:44 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (46.16%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] transpose(int[][] arr) {
+        int m = arr.length ;
+        int n = arr[0].length ;
+        int [][] transpose = new int [n][m] ;
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                transpose[i][j] = arr[j][i];
+            }
+        }
+        return transpose ;
+        
+    }
+}
