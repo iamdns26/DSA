cdf7607205eb504476bb6a341819259b1fc737e4
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-05-27 05:45:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (36.25%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,14 +1,18 @@
 class Solution {
-     public int robHelper(int[] nums, int idx, int[] dp) {
+    public int robHelper(int idx, int free, int[] nums, int[][] dp) {
         if(idx >= nums.length) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        int take = nums[idx] + robHelper(nums,idx+2,dp);
-        int skip = robHelper(nums,idx+1,dp);
-        return dp[idx] = Math.max(take,skip);
+        if(dp[idx][free] != -1) return dp[idx][free];
+        if(free == 0) return robHelper(idx + 1, 1, nums,dp);
+
+        int take = nums[idx] + robHelper(idx + 1, 0, nums,dp);
+        int skip = robHelper(idx + 1, 1, nums,dp);
+
+        return dp[idx][free] = Math.max(take , skip);
     }
+
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return robHelper(nums,0,dp);
+        int[][] dp = new int[nums.length+1][2];
+       for(int[] d : dp) Arrays.fill(d,-1);
+        return robHelper(0,1,nums,dp);
     }
 }
