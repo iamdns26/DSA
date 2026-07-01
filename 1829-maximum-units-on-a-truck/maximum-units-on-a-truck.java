ee249f5e9b7341f014f5aad12b7045517cf14633
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-07-01 16:48:46 +0530

    Time: 9 ms (95.89%) | Memory: 47 MB (44.54%) - LeetSync

---
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maximumUnits(int[][] box, int size) {
+        Arrays.sort(box, (a, b) -> Integer.compare(a[1], b[1]));
+        int ans = 0;
+        int n = box.length;
+
+        for(int i = n-1 ; i >= 0 ; i--){
+            
+            int noOfBox = box[i][0];
+            int wetOfBox = box[i][1];
+            
+            if(noOfBox <= size){
+                size -= noOfBox;
+                ans += noOfBox * wetOfBox;
+            }
+            else if(size > 0){
+                ans += size * wetOfBox;
+                break;
+            } 
+            
+        }
+        return ans;
+    }
+}
