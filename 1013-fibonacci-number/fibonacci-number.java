6090fd03005cb93268abe857bf8a23b759594910
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-07-01 10:18:12 +0530

    Time: 32 ms (8.72%) | Memory: 46.3 MB (15.73%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,8 @@
 class Solution {
+    int[] dp ;
     public int fib(int n) {
+        dp =  new int[n+1];
         if(n==0 || n==1) return n;
-        int[] dp = new int[n+1];
         if(dp[n] != 0) return dp[n];
         return dp[n] = fib(n-1)+fib(n-2);
     }
