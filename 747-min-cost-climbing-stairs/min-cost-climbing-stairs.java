5d926a3b400c82eb9edb88e4d5ff46c5d1a9af1b
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-07-01 12:33:51 +0530

    Time: 1 ms (39.41%) | Memory: 44.8 MB (58.81%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -2,12 +2,15 @@ class Solution {
     int n;
     int[] dp;
     public int helper(int i, int[] cost) {
-        //if(i>n) return Integer.MAX_VALUE;
         if(i==n) return 0;
+        if(i > n) {
+            return Integer.MAX_VALUE - cost[i-2];
+        }
         if(dp[i] != -1) return dp[i];
         int v1 = cost[i] + helper(i+1, cost);
-        int v2 =  Integer.MAX_VALUE;
-        if(i <= n-2) v2 = cost[i] + helper(i+2, cost);
+        int v2 = cost[i] + helper(i+2, cost);
+        // int v2 =  Integer.MAX_VALUE;
+        // if(i <= n-2) v2 = cost[i] + helper(i+2, cost);
         return dp[i] = Math.min(v1,v2);
     }
 
