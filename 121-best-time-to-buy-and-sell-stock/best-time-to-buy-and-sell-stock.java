828f38173073a4c4bf5f6e4bdf211be0d35b926e
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-07-01 17:03:56 +0530

    Time: 1 ms (99.94%) | Memory: 94.6 MB (32.51%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,12 @@
 class Solution {
-    public int maxProfit(int[] arr) {
+    public int maxProfit(int[] prices) {
+        int maxProfit = Integer.MIN_VALUE;
         int bestBuy = Integer.MAX_VALUE;
-        int bestProfit = Integer.MIN_VALUE;
-
-        for(int i = 0 ; i < arr.length ; i++){
-            bestBuy = Math.min(arr[i],bestBuy);
-            int pro = arr[i] - bestBuy ;
-            bestProfit = Math.max(pro,bestProfit);
+        for(int ele : prices){
+             bestBuy = Math.min(ele,bestBuy);
+            int profit = Math.abs(bestBuy - ele);
+            maxProfit = Math.max(profit,maxProfit);
         }
-
-        return bestProfit;
+        return maxProfit;
     }
 }
