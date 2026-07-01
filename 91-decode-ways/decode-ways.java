34ce220f1b0d9657891a37a98856690caed9588b
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-07-01 15:25:17 +0530

    Time: 1 ms (81.41%) | Memory: 43.3 MB (23.51%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,28 +1,27 @@
 class Solution {
-
-    public int helper(String s, int idx, int[] dp) {
+    int[] dp;
+    public int helper(int idx, String s) {
 
         if (idx == s.length()) return 1;
         if (s.charAt(idx) == '0') return 0;
-        if(dp[idx] != -1) return dp[idx]; 
+        if(dp[idx] != -1) return dp[idx];
 
-        int ways = 0;     
+        int ways = helper(idx + 1, s);
 
-        int oneDigit = Integer.parseInt(s.substring(idx, idx + 1));
-        if (oneDigit >= 1 && oneDigit <= 26) {
-            ways += helper(s, idx + 1, dp);
-        }
         if (idx + 1 < s.length()) {
-            int twoDigit = Integer.parseInt(s.substring(idx, idx + 2));
-            if (twoDigit >= 10 && twoDigit <= 26) {
-                ways += helper(s, idx + 2, dp);
+            int two = Integer.parseInt(s.substring(idx, idx + 2));
+
+            if (two >= 10 && two <= 26) {
+                ways += helper(idx + 2, s);
             }
         }
+
         return dp[idx] = ways;
     }
+
     public int numDecodings(String s) {
-        int[] dp = new int[s.length()];
+        dp = new int[s.length()];
         Arrays.fill(dp,-1);
-        return helper(s, 0, dp);
+        return helper(0, s);
     }
 }
