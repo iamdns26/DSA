69907a38a3242a32063a460cac9d19b17285aa58
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-07-02 13:00:47 +0530

    Time: 50 ms (5.37%) | Memory: 48.2 MB (5.36%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,21 +1,27 @@
 class Solution {
-    public long coinChangeHelper(int idx, int amount, int[] coins,long[][] dp) {
-        if(idx >= coins.length){
-            if(amount==0) return 0;
-            return Integer.MAX_VALUE;
-        }
-        if(dp[idx][amount] != -1) return dp[idx][amount];
+    long[][] dp;
+    public long helper(int idx, int[] arr,int amount){
         
-        long skip = coinChangeHelper(idx+1,amount,coins,dp);
-        if(amount - coins[idx] < 0) return dp[idx][amount] = skip;
-        long take = 1 + coinChangeHelper(idx,amount-coins[idx],coins,dp);
+        if(idx>=arr.length){
+            if(amount==0) return 0;
+            else return Integer.MAX_VALUE;
+        }
 
-        return  dp[idx][amount] = Math.min(take,skip);
+        if(dp[idx][amount] != -1) return dp[idx][amount];
+
+        long skip = helper(idx+1,arr,amount);
+
+        if(amount - arr[idx] < 0)return skip;
+
+        long take = 1 + helper(idx,arr,amount-arr[idx]);
+
+        return dp[idx][amount] = Math.min(take , skip);
     }
     public int coinChange(int[] coins, int amount) {
-        long[][] dp = new long[coins.length+1][amount+1];
+        dp = new long[coins.length][amount+1];
         for(long[] d : dp) Arrays.fill(d,-1);
-        int ans = (int)coinChangeHelper(0,amount,coins,dp);
-        return (ans == Integer.MAX_VALUE) ? -1 : ans;
+        int ans = (int)helper(0,coins,amount);
+        if(ans==Integer.MAX_VALUE) return -1;
+        return ans;
     }
 }
