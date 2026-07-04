17e59448ccb2061a25479d9b23c78052ebbbcf8a
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-07-04 10:04:55 +0530

    Time: 5 ms (21.18%) | Memory: 47.5 MB (32.26%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0 ; i < nums.length ; i++) {
+            set.add(nums[i]);
+        }
+        
+        for(int i = 0 ; i < nums.length ; i++){
+            if(!set.contains(i)) return i; 
+        }
+        return nums.length;
+    }
+}
