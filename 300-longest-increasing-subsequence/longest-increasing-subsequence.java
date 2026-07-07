913168fae2e499381ce8d938d810cbe15ce3604e
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-07-07 13:08:25 +0530

    Time: 433 ms (5.08%) | Memory: 237 MB (18.19%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,19 +1,20 @@
 class Solution {
-    public int lengthOfLISHelper(int idx, int prev, int[] nums, int[][] dp) {
-        if(idx >= nums.length) return 0;
-        if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
-
-        int pick = 0;
-        if(prev == -1 || nums[idx] > nums[prev]){
-            pick = 1 + lengthOfLISHelper(idx+1,idx,nums,dp);
+    int[][] dp;
+    public int helper(int idx, int pre , int[] arr){
+        if(idx >= arr.length) return 0;
+        int take = -1;
+       if(dp[idx][pre+1] != -1) return dp[idx][pre+1];
+        if(pre == -1 || arr[idx] > arr[pre]){
+             take = 1 + helper(idx+1,idx,arr);
         }
-        int skip = lengthOfLISHelper(idx+1,prev,nums,dp);
-
-        return dp[idx][prev+1] = Math.max(pick,skip);
+        
+        int skip =  helper(idx+1,pre,arr);
+     
+        return dp[idx][pre+1] = Math.max(take,skip);
     }
     public int lengthOfLIS(int[] nums) {
-        int [][] dp = new int[nums.length+1][nums.length+1];
-        for(int[] d : dp ) Arrays.fill(d,-1);
-        return lengthOfLISHelper(0,-1,nums, dp);
+        dp = new int[nums.length][nums.length+1];
+        for(int [] d : dp) Arrays.fill(d,-1);
+        return helper(0,-1,nums);
     }
 }
