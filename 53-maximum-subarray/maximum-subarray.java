44156472fdd8b0463412b82b83d4b495007d0249
iamdns26 <157233914+iamdns26@users.noreply.github.com>
2026-07-11 00:22:12 +0530

    Time: 1 ms (99.96%) | Memory: 77.3 MB (31.89%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -4,10 +4,8 @@ class Solution {
         int sum = 0;
         for(int ele : nums){
             sum+=ele;
-            
             if(sum>=maxSum) maxSum = sum;
             if(sum<0) sum = 0;
-            
         }
         return maxSum;
     }
